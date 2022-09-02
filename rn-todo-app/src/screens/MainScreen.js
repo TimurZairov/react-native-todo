757681e339a3caf67f3cc315ef43a00336e749d4
Timur <zairovne@gmail.com>
2022-09-02 18:18:57 +0500

    add Image tag for empty screen

--- rn-todo-app/src/screens/MainScreen.js
+++ rn-todo-app/src/screens/MainScreen.js
@@ -1,20 +1,43 @@
 import React from "react";
 import {AddTodo} from "../components/AddTodo";
-import {FlatList} from "react-native";
+import {FlatList, Image, StyleSheet, View} from "react-native";
 import {Todo} from "../components/Todo";
 
 
-export const MainScreen = ({ todos, setTodos, openTodo, removeTodo }) => {
+export const MainScreen = ({todos, setTodos, openTodo, removeTodo}) => {
+
+    let content = <FlatList
+        keyExtractor={item => item.id}
+        data={todos}
+        renderItem={({item}) => {
+            return <Todo todo={item} openTodo={openTodo} removeTodo={removeTodo}/>
+        }}/>
+
+    if (todos.length === 0) {
+        content = <View style={styles.container}>
+            <Image style={styles.image} source={require('../../assets/not-contetn.jpg')}/>
+        </View>
+    }
+
     return (
         <>
             <AddTodo setTodos={setTodos} todos={todos}/>
-            <FlatList
-                keyExtractor={item => item.id}
-                data={todos}
-                renderItem={({item}) => {
-                    return <Todo todo={item} openTodo={openTodo} removeTodo={removeTodo}/>
-                }}/>
+            {content}
         </>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 100
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    }
+})
